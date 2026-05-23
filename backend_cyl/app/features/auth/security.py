476d318997bac562d9a1ca8b33497bc40eab7e00
AlphonseCYL <2782552142@qq.com>
2026-05-23 15:42:00 +0800

    feat:完善账户密码功能

--- backend_cyl/app/features/auth/security.py
+++ backend_cyl/app/features/auth/security.py
@@ -13,13 +13,15 @@ from app.core.config import (
     JWT_SECRET_KEY,
 )
 
-
+# 生成一个随机salt，再根据密码生成hash值，输出格式为salt$hash
 def hash_password(password: str) -> str:
     salt = os.urandom(16).hex()
     digest = hashlib.pbkdf2_hmac("sha256", password.encode(), salt.encode(), 100_000)
     return f"{salt}${digest.hex()}"
 
-
+# 验证密码是否正确，输入密码和数据库中存储的salt$hash值
+# 根据输入密码以及salt来计算一个hash值，并与数据库中的hash值进行比较
+# 返回True或False
 def verify_password(password: str, password_hash: str) -> bool:
     try:
         salt, expected_digest = password_hash.split("$", 1)
